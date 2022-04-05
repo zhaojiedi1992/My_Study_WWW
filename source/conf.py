4bdd5d8b0afef544d2dc50f92956d0e09f1102b5
zhaojiedi <zhaojiedi@bytedance.com>
2022-04-05 18:07:17 +0800

    cm

--- source/conf.py
+++ source/conf.py
@@ -59,6 +59,11 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+  'logo_only': True,
+  'disply_version': False,
+  'collapse_navigation': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
